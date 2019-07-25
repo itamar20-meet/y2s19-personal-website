2a33498f36ed8aed166c76f4509ebb18747ac6a4
myname21-meet <myname21@meet.mit.edu>
2019-07-25 11:33:39 -0400

    not close to finishing

---
+++ personal-website.js
@@ -0,0 +1,17 @@
+// Write all your JavaScript and JQuery code in this file! :)
+/* START PROVIDED CODE */
+function randomX {
+  return Math.random() * $(window).width();
+
+
+function randomY() {
+  return Math.random() * $(window).height();
+}
+/* END PROVIDED CODE */
+
+// Write your code below this line :)
+$("#moveButton").on("click",randomX(), randomY()){
+	$("#message").css("left",randomX)
+	$("#message").css("top", randomY)
+	$("#message").css("position", relative)
+}
